40495fe7d1ed0edf706432729d1d331a37ded6a7
Justin Horner <justinh.horner@gmail.com>
2025-03-23 02:33:58 -0400

    Add installer for gum

---
+++ install/terminal/app-gum.ps1
@@ -0,0 +1,13 @@
+# Gum is used for the to tailor Omakwin after the initial install
+$TMP_PATH = "$env:TMP\omakwin"
+$GUM_VERSION="0.14.3"
+$GUM_PATH="gum_${GUM_VERSION}_Windows_x86_64"
+
+if (Test-Path -Path $TMP_PATH\gum.exe) {
+    return
+}
+
+Invoke-WebRequest -Uri "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/${GUM_PATH}.zip" -OutFile "$TMP_PATH\gum.zip"
+Expand-Archive -Path "$TMP_PATH\gum.zip" -DestinationPath "$TMP_PATH\gum"
+Move-Item -Path "$TMP_PATH\gum\$GUM_PATH\gum.exe" -Destination "$TMP_PATH"
+Remove-Item -Path "$TMP_PATH\*" -Exclude "gum.exe" -Recurse
