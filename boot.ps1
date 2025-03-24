1135b1c3776ea88289c2d1422035528e48c6b9ec
Justin Horner <justinh.horner@gmail.com>
2025-03-23 21:18:29 -0400

    Fix formatting

--- boot.ps1
+++ boot.ps1
@@ -16,29 +16,28 @@ $OMAKWIN_LOGO='________                  __           .__
         \/      \/     \/     \/               \/ 
                 Peak Windows for Game Programmers'
 
-    $LOGO_COLORS = @(
-    "DarkBlue",
-    "DarkBlue",
-    "Blue",
-    "Blue",
-    "Blue",
-    "DarkBlue",
-    "White"
-    )
+$LOGO_COLORS = @(
+"DarkBlue",
+"DarkBlue",
+"Blue",
+"Blue",
+"Blue",
+"DarkBlue",
+"White"
+)
 
-    $WIN_LOGO = $WIN_LOGO -Split "`n"
-    $OMAKWIN_LOGO = $OMAKWIN_LOGO -Split "`n"
+$WIN_LOGO = $WIN_LOGO -Split "`n"
+$OMAKWIN_LOGO = $OMAKWIN_LOGO -Split "`n"
 
-    for ($LINE_INDEX = 0; $LINE_INDEX -lt $OMAKWIN_LOGO.Length; $LINE_INDEX++) {
-        $COLOR = $LOGO_COLORS[$LINE_INDEX]
-        Write-Host -ForegroundColor $COLOR $OMAKWIN_LOGO[$LINE_INDEX]
+for ($LINE_INDEX = 0; $LINE_INDEX -lt $OMAKWIN_LOGO.Length; $LINE_INDEX++) {
+    $COLOR = $LOGO_COLORS[$LINE_INDEX]
+    Write-Host -ForegroundColor $COLOR $OMAKWIN_LOGO[$LINE_INDEX]
 
-        $WIN_LOGO_DISTANCE = ("`t    " * 6) -Join ""
-        Write-Host -NoNewline $WIN_LOGO_DISTANCE
-        Write-Host -NoNewline -ForegroundColor Blue $WIN_LOGO[$LINE_INDEX]
-    }
-
-    Write-Output "`n"
+    $WIN_LOGO_DISTANCE = ("`t    " * 6) -Join ""
+    Write-Host -NoNewline $WIN_LOGO_DISTANCE
+    Write-Host -NoNewline -ForegroundColor Blue $WIN_LOGO[$LINE_INDEX]
+}
+Write-Output "`n"
 
 Write-Host "Omakwin is for fresh Windows 11 installations only!"
 Write-Host "Elevated privileges required."
